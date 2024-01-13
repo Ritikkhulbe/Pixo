fd8403d09ea88afbf62c889ecb1966c9b24ca029
Ritik <91615440+Ritikkhulbe@users.noreply.github.com>
2024-01-13 11:54:55 +0530

    Initial commit

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+const SignupForm = () => {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default SignupForm
