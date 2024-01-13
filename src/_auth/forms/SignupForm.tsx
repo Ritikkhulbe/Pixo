17fe7245e5a38cdb524894a2b5878a1958f92343
Ritik <91615440+Ritikkhulbe@users.noreply.github.com>
2024-01-13 16:05:37 +0530

    Connected Database to signup
    Appwrite connection to signUp

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,18 +1,23 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import * as z from "zod";
+import { Link } from 'react-router-dom';
 
 
 import { Button } from "@/components/ui/button"
-import { FormControl,Form,FormDescription,FormField,FormItem,FormLabel,FormMessage } from "@/components/ui/form"
+import { FormControl,Form,FormField,FormItem,FormLabel,FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { useForm } from "react-hook-form";
 import { SignupValidation } from "@/lib/validation";
 import Loader from "@/components/shared/Loader";
+import { useToast } from "@/components/ui/use-toast";
+import { useCreateUserAccount } from "@/lib/react-query/queryAndMutations";
 
 
 const SignupForm = () => {
+  const {toast} = useToast();
 
-  const isLoading = true;
+
+  //const { mutateAsync : createUserAccount , isLoading :isCreatingAccount } = useCreateUserAccount();
 
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
@@ -24,18 +29,27 @@ const SignupForm = () => {
     },
   })
  
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    const newUser = await createUserAccount(values);
+    
+    if(!newUser){
+      return toast({
+        title: 'Sign up failed. Please try again.'
+      });
+    }
+
+    //const session = await signInAccount();
+
+
   }
 
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo.svg" alt="logo" />
-
+       
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
-          To use Pixo enter your account details
+          To use Pixo, please enter your account details
         </p>
       
         <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col
@@ -93,11 +107,17 @@ const SignupForm = () => {
               )}
             />
           <Button type="submit" className="shad-button_primary">
-            {isLoading ? (
+            {true ? (
               <div className="flex-center gap-2">
                 <Loader /> Loading...
               </div>
-            ) : "Sign up"}</Button>      
+            ) : "Sign up"}
+          </Button>
+          <p className="text-small-regular text-light-2 text-center mt-2">
+              Already have an account?
+              <Link to="/sign-in" className="text-primary-500
+              text-small-semibold ml-1">Log in</Link>
+          </p>      
         </form>
       </div>
    </Form>
--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -2,7 +2,8 @@
 const Loader = () => {
   return (
     <div className="flex-center w-full">
-        <img src="/assests/icons/loader.svg" alt="loading" width={24} height={24} />
+        <img src="/assets/icons/loader.svg" 
+        alt="loading" width={24} height={24} />
         
     </div>
   )
