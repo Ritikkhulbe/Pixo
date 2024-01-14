4306309819260d029e706800ef91e30793b12a20
Ritik <91615440+Ritikkhulbe@users.noreply.github.com>
2024-01-14 12:36:08 +0530

    Routing to all navs

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 
 import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
-import { Home } from './_root/pages';
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
 import { Toaster } from './components/ui/toaster';
